85713f137ab1cd69981a947431dbbc311bfedee9
kalibani <kalibani.ka@gmail.com>
2024-07-29 00:27:12 +0700

    implement custom rendering

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,25 @@ const options: Option[] = [
   { value: "4", label: "long long long long option 4" },
   { value: "5", label: "long long long long long option 5" },
   { value: "6", label: "long long long long long long option 6" },
-  { value: "7", label: "long long long long long long option 7" },
+  // Add more options as needed
 ];
+
+const customOptions: Option = [
+  { value: "Abe", label: "Abe", customAbbreviation: "A" },
+  { value: "John", label: "John", customAbbreviation: "J" },
+  { value: "Dustin", label: "Dustin", customAbbreviation: "D" },
+];
+
+const customRenderOption = ({ label, customAbbreviation }: Option) => (
+  <div
+    style={{ display: "flex", justifyContent: "space-between", width: "100%" }}
+  >
+    <div>{label}</div>
+    <div style={{ marginLeft: "10px", color: "#ccc" }}>
+      {customAbbreviation}
+    </div>
+  </div>
+);
 
 const App: FC = () => {
   const handleMultiSelectChange = (selected: Option | Option[]) => {
@@ -20,18 +37,34 @@ const App: FC = () => {
   };
 
   return (
-    <div className="p-4 max-w-screen-xl">
-      <h1 className="text-2xl mb-4">Select Component</h1>
-      <Select
-        options={options}
-        onChange={handleMultiSelectChange}
-        multiple={true}
-        withSearch={true}
-        portal={true}
-        zIndex={1000}
-        outlined={false}
-      />
-    </div>
+    <>
+      <div className="p-4 max-w-screen-xl">
+        <h1 className="text-2xl mb-4">Select Component</h1>
+        <Select
+          options={options}
+          onChange={handleMultiSelectChange}
+          multiple={true}
+          withSearch={true}
+          portal={false}
+          zIndex={1000}
+          outlined={false}
+        />
+      </div>
+
+      <div className="p-4 max-w-screen-xl">
+        <h1 className="text-2xl mb-4">Custom Option Rendering</h1>
+        <Select
+          options={customOptions}
+          onChange={handleMultiSelectChange}
+          multiple={true}
+          withSearch={true}
+          portal={false}
+          zIndex={1000}
+          outlined={false}
+          customRenderOption={customRenderOption}
+        />
+      </div>
+    </>
   );
 };
 
